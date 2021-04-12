71255c5b97642c33e9f618fba08dc84659e5c4a9
Prashant Bharti <46451926+prashantiitv@users.noreply.github.com>
2021-04-12 15:43:20 +0530

    Update ouical.js

--- js/vendor/ouical.js
+++ js/vendor/ouical.js
@@ -30,76 +30,76 @@
                 href + '">Google Calendar</a>';
         },
 
-        yahoo: function(event) {
-            var eventDuration = event.end ?
-                ((event.end.getTime() - event.start.getTime())/ MS_IN_MINUTES) :
-                event.duration;
+//         yahoo: function(event) {
+//             var eventDuration = event.end ?
+//                 ((event.end.getTime() - event.start.getTime())/ MS_IN_MINUTES) :
+//                 event.duration;
 
-            // Yahoo dates are crazy, we need to convert the duration from minutes to hh:mm
-            var yahooHourDuration = eventDuration < 600 ?
-                '0' + Math.floor((eventDuration / 60)) :
-                Math.floor((eventDuration / 60)) + '';
+//             // Yahoo dates are crazy, we need to convert the duration from minutes to hh:mm
+//             var yahooHourDuration = eventDuration < 600 ?
+//                 '0' + Math.floor((eventDuration / 60)) :
+//                 Math.floor((eventDuration / 60)) + '';
 
-            var yahooMinuteDuration = eventDuration % 60 < 10 ?
-                '0' + eventDuration % 60 :
-                eventDuration % 60 + '';
+//             var yahooMinuteDuration = eventDuration % 60 < 10 ?
+//                 '0' + eventDuration % 60 :
+//                 eventDuration % 60 + '';
 
-            var yahooEventDuration = yahooHourDuration + yahooMinuteDuration;
+//             var yahooEventDuration = yahooHourDuration + yahooMinuteDuration;
 
-            // Remove timezone from event time
-            var st = formatTime(new Date(event.start - (event.start.getTimezoneOffset() *
-                MS_IN_MINUTES))) || '';
+//             // Remove timezone from event time
+//             var st = formatTime(new Date(event.start - (event.start.getTimezoneOffset() *
+//                 MS_IN_MINUTES))) || '';
 
-            var href = encodeURI([
-                'http://calendar.yahoo.com/?v=60&view=d&type=20',
-                '&title=' + (event.title || ''),
-                '&st=' + st,
-                '&dur=' + (yahooEventDuration || ''),
-                '&desc=' + (event.description || ''),
-                '&in_loc=' + (event.address || '')
-            ].join(''));
+//             var href = encodeURI([
+//                 'http://calendar.yahoo.com/?v=60&view=d&type=20',
+//                 '&title=' + (event.title || ''),
+//                 '&st=' + st,
+//                 '&dur=' + (yahooEventDuration || ''),
+//                 '&desc=' + (event.description || ''),
+//                 '&in_loc=' + (event.address || '')
+//             ].join(''));
 
-            return '<a class="icon-yahoo" target="_blank" href="' +
-                href + '">Yahoo! Calendar</a>';
-        },
+//             return '<a class="icon-yahoo" target="_blank" href="' +
+//                 href + '">Yahoo! Calendar</a>';
+//         },
 
-        ics: function(event, eClass, calendarName) {
-            var startTime = formatTime(event.start);
-            var endTime = calculateEndTime(event);
+//         ics: function(event, eClass, calendarName) {
+//             var startTime = formatTime(event.start);
+//             var endTime = calculateEndTime(event);
 
-            var href = encodeURI(
-                'data:text/calendar;charset=utf8,' + [
-                    'BEGIN:VCALENDAR',
-                    'VERSION:2.0',
-                    'BEGIN:VEVENT',
-                    'URL:' + document.URL,
-                    'DTSTART:' + (startTime || ''),
-                    'DTEND:' + (endTime || ''),
-                    'SUMMARY:' + (event.title || ''),
-                    'DESCRIPTION:' + (event.description || ''),
-                    'LOCATION:' + (event.address || ''),
-                    'END:VEVENT',
-                    'END:VCALENDAR'].join('\n'));
+//             var href = encodeURI(
+//                 'data:text/calendar;charset=utf8,' + [
+//                     'BEGIN:VCALENDAR',
+//                     'VERSION:2.0',
+//                     'BEGIN:VEVENT',
+//                     'URL:' + document.URL,
+//                     'DTSTART:' + (startTime || ''),
+//                     'DTEND:' + (endTime || ''),
+//                     'SUMMARY:' + (event.title || ''),
+//                     'DESCRIPTION:' + (event.description || ''),
+//                     'LOCATION:' + (event.address || ''),
+//                     'END:VEVENT',
+//                     'END:VCALENDAR'].join('\n'));
 
-            return '<a class="' + eClass + '" target="_blank" href="' +
-                href + '">' + calendarName + ' Calendar</a>';
-        },
+//             return '<a class="' + eClass + '" target="_blank" href="' +
+//                 href + '">' + calendarName + ' Calendar</a>';
+//         },
 
-        ical: function(event) {
-            return this.ics(event, 'icon-ical', 'iCal');
-        },
+//         ical: function(event) {
+//             return this.ics(event, 'icon-ical', 'iCal');
+//         },
 
-        outlook: function(event) {
-            return this.ics(event, 'icon-outlook', 'Outlook');
-        }
+//         outlook: function(event) {
+//             return this.ics(event, 'icon-outlook', 'Outlook');
+//         }
     };
 
     var generateCalendars = function(event) {
         return {
             google: calendarGenerators.google(event),
-            yahoo: calendarGenerators.yahoo(event),
-            ical: calendarGenerators.ical(event),
-            outlook: calendarGenerators.outlook(event)
+//             yahoo: calendarGenerators.yahoo(event),
+//             ical: calendarGenerators.ical(event),
+//             outlook: calendarGenerators.outlook(event)
         };
     };
 
